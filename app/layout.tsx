6d91cf6b596ef5c2df29cd7e5eb677197508fda5
Julian Neagu <neiulian@gmail.com>
2026-03-04 02:36:30 +0000

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import './globals.css'
-export const metadata = { title: 'makesworth-nextjs', description: 'Converted by VEX Studio' }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
